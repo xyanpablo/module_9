0bf1bccc2094366a02c7d4d3b1e33b5e901d9d40
xyanpablo <xyanpablo671@gfmail.com>
2024-11-04 23:01:48 +0300

    m9

--- module_9_3.py
+++ module_9_3.py
@@ -2,7 +2,8 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (abs(len(i) - len(j)) for i, j in zip(first, second) if len(i) != len(j))
-second_result = (len(i) == len(j) for i in first for j in second if first.index(i) == second.index(j))
+# second_result = (len(i) == len(j) for i in first for j in second if first.index(i) == second.index(j))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
 print(list(first_result))
 print(list(second_result))
